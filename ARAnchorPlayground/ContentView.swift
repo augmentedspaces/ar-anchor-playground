490bd110ee165927b922388fbe515c9f0c0ad72c
nien <nien@linebreak.studio>
2021-10-15 14:28:42 -0400

    Cleanup and refactor. Added reset logic.

--- ARAnchorPlayground/ContentView.swift
+++ ARAnchorPlayground/ContentView.swift
@@ -13,12 +13,11 @@ import Combine
 
 // MARK: - View model for handling communication between the UI and ARView.
 class ViewModel: ObservableObject {
+
     let uiSignal = PassthroughSubject<UISignal, Never>()
-    
-    // Variable for tracking ambient light intensity.
-    @Published var ambientIntensity: Double = 0
 
     enum UISignal {
+        case reset
     }
 }
 
@@ -31,6 +30,19 @@ struct ContentView : View {
         ZStack {
             // AR View.
             ARViewContainer(viewModel: viewModel)
+            
+            // Reset button.
+            Button {
+                viewModel.uiSignal.send(.reset)
+            } label: {
+                Label("Reset", systemImage: "gobackward")
+                    .font(.system(.title2).weight(.medium))
+                    .foregroundColor(.white)
+                    .labelStyle(IconOnlyLabelStyle())
+                    .frame(width: 30, height: 30)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding()
         }
         .edgesIgnoringSafeArea(.all)
         .statusBar(hidden: true)
@@ -52,13 +64,13 @@ struct ARViewContainer: UIViewRepresentable {
 class SimpleARView: ARView, ARSessionDelegate {
     var viewModel: ViewModel
     var arView: ARView { return self }
+    var originAnchor: AnchorEntity!
+    var pov: AnchorEntity!
     var subscriptions = Set<AnyCancellable>()
-    
+
     // Dictionary for tracking image anchors.
     var imageAnchorToEntity: [ARImageAnchor: AnchorEntity] = [:]
-    
-    // Example box entity.
-    var boxEntity: ModelEntity!
+
 
     init(frame: CGRect, viewModel: ViewModel) {
         self.viewModel = viewModel
@@ -79,32 +91,40 @@ class SimpleARView: ARView, ARSessionDelegate {
         UIApplication.shared.isIdleTimerDisabled = true
         
         setupScene()
+        
+        setupEntities()
     }
     
+
     func setupScene() {
         // Setup world tracking and image detection.
-        let configuration = ARImageTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         arView.renderOptions = [.disableDepthOfField, .disableMotionBlur]
 
-        let targetImage    = "itp-logo.jpg"
-        let physicalWidth  = 0.1524
-        
-        if let refImage = UIImage(named: targetImage)?.cgImage {
+        // Create set hold target image references.
+        var set = Set<ARReferenceImage>()
 
-            let arReferenceImage = ARReferenceImage(refImage, orientation: .up, physicalWidth: physicalWidth)
-
-            // Assign name to image reference.
-            arReferenceImage.name = "Image Alpha"
-
-            var set = Set<ARReferenceImage>()
-            set.insert(arReferenceImage)
-            configuration.trackingImages = set
-        } else {
-            print("❗️ Error loading target image")
+        // Setup target image A.
+        if let detectionImage = makeDetectionImage(named: "itp-logo.jpg",
+                                                   referenceName: "IMAGE_ALPHA",
+                                                   physicalWidth: 0.18415) {
+            set.insert(detectionImage)
         }
-        
 
-        arView.session.run(configuration)
+        // Setup target image B.
+        if let detectionImage = makeDetectionImage(named: "dino.jpg",
+                                                   referenceName: "IMAGE_BETA",
+                                                   physicalWidth: 0.19) {
+            set.insert(detectionImage)
+        }
+
+
+        // Add target images to configuration.
+        configuration.detectionImages = set
+        configuration.maximumNumberOfTrackedImages = 2
+
+        // Run configuration.
+        arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
         
         // Called every frame.
         scene.subscribe(to: SceneEvents.Update.self) { event in
@@ -119,55 +139,101 @@ class SimpleARView: ARView, ARSessionDelegate {
         // Set session delegate.
         arView.session.delegate = self
     }
-    
-    // Hide/Show active tetromino.
-    func processUISignal(_ signal: ViewModel.UISignal) {
+
+
+    // Helper method for creating a detection image.
+    func makeDetectionImage(named: String, referenceName: String, physicalWidth: CGFloat) -> ARReferenceImage? {
+        guard let targetImage = UIImage(named: named)?.cgImage else {
+            print("❗️ Error loading target image:", named)
+            return nil
+        }
+
+        let arReferenceImage  = ARReferenceImage(targetImage, orientation: .up, physicalWidth: physicalWidth)
+        arReferenceImage.name = referenceName
+
+        return arReferenceImage
     }
 
+
+    // Process UI signals.
+    func processUISignal(_ signal: ViewModel.UISignal) {
+        switch signal {
+        case .reset:
+            print("👇 Did press reset button")
+            
+            // Reset scene and all anchors.
+            arView.scene.anchors.removeAll()
+            subscriptions.removeAll()
+            
+            setupScene()
+            setupEntities()
+        }
+    }
+
+
+    // Called when an anchor is added to scene.
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        // Handle image anchors.
         anchors.compactMap { $0 as? ARImageAnchor }.forEach {
+            // Grab reference image name.
+            guard let referenceImageName = $0.referenceImage.name else { return }
 
-            if let imageName = $0.referenceImage.name {
-                // Extract name when image anchor is detected / set.
-                print(imageName)
-            }
-
-            // Create anchor from image.
-            let anchorEntity = AnchorEntity(anchor: $0)
-            
-            // Track image anchors added to scene.
-            imageAnchorToEntity[$0] = anchorEntity
-            
-            // Add anchor to scene.
+            // Create anchor and place at image location.
+            let anchorEntity = AnchorEntity(world: $0.transform)
             arView.scene.addAnchor(anchorEntity)
             
-            // Call setup method for entities.
-            // IMPORTANT: Play USDZ animations after entity is added to the scene.
-            setupEntities(anchorEntity: anchorEntity)
+            // Setup logic based on reference image.
+            if referenceImageName == "IMAGE_ALPHA" {
+                setupEntitiesForImageAlpha(anchorEntity: anchorEntity)
+            } else if referenceImageName == "IMAGE_BETA" {
+                setupEntitiesForImageBeta(anchorEntity: anchorEntity)
+            }
         }
     }
 
 
-    func setupEntities(anchorEntity: AnchorEntity) {
-        // Checker material.
-        var checkerMaterial = PhysicallyBasedMaterial()
-        let texture = PhysicallyBasedMaterial.Texture.init(try! .load(named: "checker.png"))
-        checkerMaterial.baseColor.texture = texture
+    // Setup method for non image anchor entities.
+    func setupEntities() {
+        // Create an anchor at scene origin.
+        originAnchor = AnchorEntity(world: .zero)
+        arView.scene.addAnchor(originAnchor)
 
-        // Setup example box entity.
-        let boxMesh = MeshResource.generateBox(size: [0.1, 0.1, 0.1], cornerRadius: 0.0)
-        boxEntity = ModelEntity(mesh: boxMesh, materials: [checkerMaterial])
-
-        // Position and add box entity to anchor.
-        boxEntity.position.y = 0.05
-        anchorEntity.addChild(boxEntity)
+        // Add yellow marker to origin anchor.
+        originAnchor.addChild(makeBoxMarker(color: .yellow))
+        
+        // Add pov entity that follows the camera.
+        pov = AnchorEntity(.camera)
+        arView.scene.addAnchor(pov)
     }
-    
 
+
+    // IMPORTANT: Attach to anchor entity. Called when image target is found.
+
+    func setupEntitiesForImageAlpha(anchorEntity: AnchorEntity) {
+        // Add red marker to alpha anchor.
+        let marker = makeBoxMarker(color: .red)
+        anchorEntity.addChild(marker)
+
+    }
+
+    func setupEntitiesForImageBeta(anchorEntity: AnchorEntity) {
+        // Add green marker to beta anchor.
+        let marker = makeBoxMarker(color: .green)
+        anchorEntity.addChild(marker)
+
+    }
+
+
+    // Render loop.
     func renderLoop() {
-        // Spin boxEntity if available.
-        if let box = boxEntity {
-            box.orientation *= simd_quatf(angle: 0.02, axis: [0, 1, 0])
-        }
+        
+    }
+
+
+    // Helper method for making box to mark anchor position.
+    func makeBoxMarker(color: UIColor) -> Entity {
+        let boxMesh   = MeshResource.generateBox(size: 0.025, cornerRadius: 0.002)
+        let material  = SimpleMaterial(color: color, isMetallic: false)
+        return ModelEntity(mesh: boxMesh, materials: [material])
     }
 }
